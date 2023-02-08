d0ea870d6c1ff0590008da27ac7a70c1b5cf0ca9
dudi2022 <beattywilliam2020@gmail.com>
2023-02-08 19:48:39 +0100

    pop-up that displays title and abstract

---
+++ src/Modal.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const Modal = ({
+  selectedInventionTitle,
+  selectedInventionAbstract,
+  handleClose,
+}) => {
+  const [showModal, setShowModal] = useState(true);
+
+  const handleClickOutside = (event) => {
+    if (event.target.className === "modal-overlay") {
+      setShowModal(false);
+    }
+  };
+
+  return (
+    <div>
+      {showModal && (
+        <div className="modal-overlay" onClick={handleClickOutside}>
+          <div className="modal-content">
+            <h3>Abstract</h3>
+            <p>{selectedInventionTitle}</p>
+            <p>{selectedInventionAbstract}</p>
+          </div>
+            <button className="modal-button" onClick={handleClose}>Close</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Modal;
--- src/ReactGrid.jsx
+++ src/ReactGrid.jsx
@@ -1,62 +1,115 @@
 import React, { useState } from "react";
 import "./App.css";
 import PageSelector from "./PageSelector";
+import Modal from "./Modal";
 
 function ReactGrid({ data, error }) {
-const [currentPage, setCurrentPage] = useState(1);
-const itemsPerPage = 12;
-const totalPages = Math.ceil(data.length / itemsPerPage);
-const startIndex = (currentPage - 1) * itemsPerPage;
-const currentData = data.slice(startIndex, startIndex + itemsPerPage);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [showPopUp, setShowPopUp] = useState(false);
+  const [selectedInventionTitle, setSelectedInventionTitle] = useState(null);
+  const [selectedInventionAbstract, setSelectedInventionAbstract] = useState(null);
+  const itemsPerPage = 12;
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const currentData = data.slice(startIndex, startIndex + itemsPerPage);
 
-const handlePageChange = (newPage) => {
-setCurrentPage(newPage);
-};
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
 
-return (
-<div className="results-table-grid-container">
-<table className="main-table">
-<thead className="header-cell">
-<tr>
-<th>Titolo</th>
-<th>Numero</th>
-<th>Autore</th>
-<th>Data</th>
-<th>LinkOPS</th>
-</tr>
-</thead>
-<tbody>
-{error ? (
-<tr>
-<td colSpan={5}>Error: {error}</td>
-</tr>
-) : (
-currentData.map((item, index) => (
-<tr className="table-cell" key={index}>
-<td style={{wordWrap:"break-word"}} >{item.invention_title}</td>
-<td style={{wordWrap:"break-word"}} >{item.doc_num}</td>
-<td style={{wordWrap:"break-word"}} >{item.inventor_name}</td>
-<td style={{wordWrap:"break-word"}} >{item.date}</td>
-<td>
-   <a style={{wordWrap:"break-word"}} href={item.ops_link} target="_blank" rel="noopener noreferrer">
-     {item.ops_link}
-   </a>
-</td>
-</tr>
-))
-)}
-</tbody>
-</table>
-<div>
-<PageSelector
-       currentPage={currentPage}
-       totalPages={totalPages}
-       handlePageChange={handlePageChange}
-       itemsPerPage={20}
-     />
-</div>
-</div>
-);
+  const handleClick = (inventionTitle, inventionAbstract) => {
+    setSelectedInventionTitle(inventionTitle);
+    setSelectedInventionAbstract(inventionAbstract);
+    setShowPopUp(true);
+  };
+
+  const handleClose = () => {
+    setShowPopUp(false);
+  };
+
+  return (
+    <div className="results-table-grid-container">
+      <table className="main-table">
+        <thead className="header-cell">
+          <tr>
+            <th>Title</th>
+            <th>Number</th>
+            <th>Author</th>
+            <th>Date</th>
+            <th>LinkOPS</th>
+          </tr>
+        </thead>
+        <tbody>
+          {error ? (
+            <tr>
+              <td colSpan={5}>Error: {error}</td>
+            </tr>
+          ) : (
+            currentData.map((item, index) => (
+              <tr className="table-cell" key={index}>
+                <td
+                  style={{ wordWrap: "break-word" }}
+                  onClick={() =>
+                    handleClick(item.invention_title, item.abstract)
+                  }
+                >
+                  {item.invention_title}
+                </td>
+                <td
+                  style={{ wordWrap: "break-word" }}
+                  onClick={() =>
+                    handleClick(item.invention_title, item.abstract)
+                  }
+                >
+                  {item.doc_num}
+                </td>
+                <td
+                  style={{ wordWrap: "break-word" }}
+                  onClick={() =>
+                    handleClick(item.invention_title, item.abstract)
+                  }
+                >
+                  {item.inventor_name}
+                </td>
+                <td
+                  style={{ wordWrap: "break-word" }}
+                  onClick={() =>
+                    handleClick(item.invention_title, item.abstract)
+                  }
+                >
+                  {item.date}
+                </td>
+                <td>
+                  <a
+                    style={{ wordWrap: "break-word" }}
+                    href={item.ops_link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {item.ops_link}
+                  </a>
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
+      {showPopUp && (
+        <Modal
+          selectedInventionTitle={selectedInventionTitle}
+          selectedInventionAbstract={selectedInventionAbstract}
+          handleClose={handleClose}
+        />
+      )}
+      <div>
+        <PageSelector
+          currentPage={currentPage}
+          totalPages={totalPages}
+          handlePageChange={handlePageChange}
+          itemsPerPage={itemsPerPage}
+        />
+      </div>
+    </div>
+  );
 }
-
 export default ReactGrid;
