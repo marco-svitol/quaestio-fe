c30cf14cd1b5067e0c00b3870a1c8217c00ae948
Marco Cambon <marco.cambon@webfleet.com>
2023-04-25 00:24:37 +0200

    unused fix

--- src/App.js
+++ src/App.js
@@ -3,64 +3,64 @@ import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Route,
-  Routes,
-  Navigate,
-  useLocation,
+  Routes//,
+  // Navigate,
+  // useLocation,
 } from "react-router-dom";
-import SearchBox from "./SearchBox";
+// import SearchBox from "./SearchBox";
 import LoginBox from "./LoginBox";
-import NavBar from "./NavBar";
-import ReactGrid from "./ReactGrid";
-import AccountPage from "./AccountPage";
-import LogoutButton from "./LogoutButton";
+// import NavBar from "./NavBar";
+// import ReactGrid from "./ReactGrid";
+// import AccountPage from "./AccountPage";
+// import LogoutButton from "./LogoutButton";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import axios from "axios";
+// import axios from "axios";
 
-function isTokenExpired(token) {
-  if (!token) return true;
+// function isTokenExpired(token) {
+//   if (!token) return true;
 
-  const currentTime = Math.floor(Date.now() / 1000);
-  const decodedToken = JSON.parse(atob(token.split(".")[1]));
-  const tokenExp = decodedToken.exp;
+//   const currentTime = Math.floor(Date.now() / 1000);
+//   const decodedToken = JSON.parse(atob(token.split(".")[1]));
+//   const tokenExp = decodedToken.exp;
 
-  return currentTime >= tokenExp;
-}
+//   return currentTime >= tokenExp;
+// }
 
-async function refreshToken() {
-  const uid = sessionStorage.getItem("uid");
-  const refToken = sessionStorage.getItem("reftoken");
-  console.log("UID:", uid);
-  console.log("RefToken:", refToken);
+// async function refreshToken() {
+//   const uid = sessionStorage.getItem("uid");
+//   const refToken = sessionStorage.getItem("reftoken");
+//   console.log("UID:", uid);
+//   console.log("RefToken:", refToken);
 
-  if (isTokenExpired(refToken)) {
-    console.log("Refresh token has expired");
-    return;
-  }
+//   if (isTokenExpired(refToken)) {
+//     console.log("Refresh token has expired");
+//     return;
+//   }
 
-  try {
-    const response = await axios.post(
-      "https://quaestio-be.azurewebsites.net/api/v1/auth/refresh",
-      {
-        uid: uid,
-        token: refToken,
-      }
-    );
+//   try {
+//     const response = await axios.post(
+//       "https://quaestio-be.azurewebsites.net/api/v1/auth/refresh",
+//       {
+//         uid: uid,
+//         token: refToken,
+//       }
+//     );
 
-    if (response.status === 200) {
-      sessionStorage.setItem("token", response.data.token);
-      console.log("Token refreshed");
-    } else {
-      console.log("Error refreshing token");
-    }
-  } catch (error) {
-    console.log("Error refreshing token:", error);
-  }
-}
+//     if (response.status === 200) {
+//       sessionStorage.setItem("token", response.data.token);
+//       console.log("Token refreshed");
+//     } else {
+//       console.log("Error refreshing token");
+//     }
+//   } catch (error) {
+//     console.log("Error refreshing token:", error);
+//   }
+// }
 function App() {
-  const [data, setData] = useState([]);
-  const [, setError] = useState(null);
+  // const [data, setData] = useState([]);
+  // const [, setError] = useState(null);
   const [, setIsLoggedIn] = useState(false);
 
   const isAuthenticated = () => {
@@ -75,14 +75,14 @@ function App() {
     }
   }, []);
 
-  const handleLogout = () => {
-    sessionStorage.clear();
-    setIsLoggedIn(false);
-  };
+  // const handleLogout = () => {
+  //   sessionStorage.clear();
+  //   setIsLoggedIn(false);
+  // };
 
   function AppContent() {
-    const location = useLocation();
-    const isLoggedIn = isAuthenticated();
+    // const location = useLocation();
+    // const isLoggedIn = isAuthenticated();
 
     // useEffect(() => {
     //   if (location.pathname !== "/login" && !isLoggedIn) {
