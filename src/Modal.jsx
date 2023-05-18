d3383293fdf57500d7ed134c58c2c63a077fa398
dudi2022 <beattywilliam2020@gmail.com>
2023-05-18 21:02:25 +0200

    cors issue now with image. lots of other updates..

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,12 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { API_BASE_URL } from "./constants";
 
 const Modal = ({
   selectedInventionTitle,
   selectedInventionAbstract,
   selectedOpsLink,
+  selectedImageLinks,
   handleClose,
 }) => {
   const [showModal, setShowModal] = useState(true);
+  const [imageData, setImageData] = useState(null);
+
+  useEffect(() => {
+    const USER_ID = sessionStorage.getItem("uid");
+
+    if (selectedImageLinks && selectedImageLinks.length > 0) {
+      fetch(
+        `${API_BASE_URL}/firstpageClipping?uid=${USER_ID}&fpcImage=${selectedImageLinks[0].link}&fpcImageFormat=${selectedImageLinks[0].format}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + sessionStorage.getItem("token"),
+          },
+        }
+      )
+        .then((response) => {
+          console.log(response.type);
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.blob();
+        })
+        .then((images) => {
+          let imgURL = images;
+          setImageData(imgURL);
+        })
+        .catch((error) => console.error("Error:", error));
+    }
+  }, [selectedImageLinks]);
 
   const handleClickOutside = (event) => {
     if (event.target.className === "modal-overlay") {
@@ -14,7 +46,6 @@ const Modal = ({
       handleClose();
     }
   };
-
   return (
     <div>
       {showModal && (
@@ -32,6 +63,11 @@ const Modal = ({
               >
                 Link OPS
               </a>
+              {imageData ? (
+                <img src={imageData} alt="document" />
+              ) : (
+                <img src="https://via.placeholder.com/150" alt="placeholder" />
+              )}
             </div>
             <button
               className="modal-button"
